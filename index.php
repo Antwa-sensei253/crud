ef65f32b37b9db858364e07724705c3180093af4
Ahmed Abdulrahim <101470839+Antwa-sensei253@users.noreply.github.com>
2024-07-09 22:07:09 +0300

    adding to github

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion Employés</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+            <table>
+            <tr id="items">
+                <th>Fname</th>
+                <th>Lname</th>
+                <th>Age</th>
+                <th>update</th>
+                <th>delete</th>
+            </tr>
+            <a href="" class="Btn_add"> <img src="images/plus.png"> Add</a>
+            <tr>
+                <th>d</th>
+                <th>dsddsd</th>
+                <th>20</th>
+               
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
